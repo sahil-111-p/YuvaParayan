61988ecee0e2185884e7c00268d41381da7f0c7d
sahil-111-p <sahilpachigar@gmail.com>
2026-07-31 23:52:46 +0530

    build

--- vite.config.ts
+++ vite.config.ts
@@ -6,6 +6,7 @@ import {defineConfig} from 'vite';
 export default defineConfig(() => {
   return {
     plugins: [react(), tailwindcss()],
+    base:'/YuvaParayan/',
     resolve: {
       alias: {
         '@': path.resolve(__dirname, '.'),
